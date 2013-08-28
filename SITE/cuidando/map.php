8ecd761b19222b87720dfd88dd012aac49a64560
Andres MRM <andresmrm@gmail.com>
2013-08-28 20:49:48 -0300

    incluindo tracking em outra pagina

--- SITE/cuidando/map.php
+++ SITE/cuidando/map.php
@@ -43,6 +43,21 @@
 		<script src="js/build-map-visualization.min.js"></script>
 	</head>
 
+	<!-- Piwik -->
+	<script type="text/javascript">
+	  var _paq = _paq || [];
+		_paq.push(["trackPageView"]);
+		_paq.push(["enableLinkTracking"]);
+
+	  (function() {
+		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.gpopai.usp.br/piwik/";
+		_paq.push(["setTrackerUrl", u+"piwik.php"]);
+		_paq.push(["setSiteId", "1"]);
+		var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+		g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<!-- End Piwik Code -->
 	<body class="preview">
 		<?php include("header.inc.php"); ?>
 
