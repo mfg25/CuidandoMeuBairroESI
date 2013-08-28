5b4ae4f8a17895bd8430a65f46c055ae1fb0d68d
Andres MRM <andresmrm@gmail.com>
2013-08-28 20:44:12 -0300

    tentando...

--- SITE/cuidando/index.php
+++ SITE/cuidando/index.php
@@ -34,25 +34,23 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/application.js"></script>
 
-	<!-- Piwik -->
-	<script type="text/javascript"> 
-	  var _paq = _paq || [];
-	  _paq.push(['trackPageView']);
-	  _paq.push(['enableLinkTracking']);
-	  (function() {
-		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.gpopai.usp.br/piwik//";
-		_paq.push(['setTrackerUrl', u+'piwik.php']);
-		_paq.push(['setSiteId', 1]);
-		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-		g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-	  })();
-
-	</script>
-	<noscript><p><img src="http://www.gpopai.usp.br/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-	<!-- End Piwik Code -->
-
   </head>
 
+	<!-- Piwik -->
+	<script type="text/javascript">
+	  var _paq = _paq || [];
+		_paq.push(["trackPageView"]);
+		_paq.push(["enableLinkTracking"]);
+
+	  (function() {
+		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.gpopai.usp.br/piwik/";
+		_paq.push(["setTrackerUrl", u+"piwik.php"]);
+		_paq.push(["setSiteId", "1"]);
+		var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+		g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<!-- End Piwik Code -->
   <body>
     <?php include("header.inc.php"); ?>
 
