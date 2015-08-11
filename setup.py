e300004500e51c8fdf780e12769baac679c00a71
Andres MRM <andres@inventati.org>
2015-08-11 14:30:32 -0300

    now in own repo

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@
 from setuptools import setup
 
 setup(
-    name="phobos",
+    name="viratoken",
     version='0.0.1',
     # url='',
-    description='Small lib to encode and decode JWTs',
+    description='Small lib to encode/decode/sign/verify JWTs '
+                'using asymmetric cryptography.',
     author='Andrés M. R. Martano',
     # author_email='',
-    packages=["phobos"],
+    packages=["viratoken"],
     install_requires=[
         'cryptography',
         'pyjwt',
