3d6a06ccb7b27c66cb27578ac4ba26712999c321
Andres MRM <andres@inventati.org>
2015-08-11 21:55:37 -0300

    better readme and ready for pypi

---
+++ README.md
@@ -0,0 +1,52 @@
+# ViraToken
+
+Small lib to encode/decode/sign/verify JWTs using asymmetric cryptography.
+Mostly a wrapper for [pyjwt](https://github.com/jpadilla/pyjwt/) for use with asymmetric cryptography.
+
+It was made for an ecosystem of microservices. One microservice produces the tokens, and the other read them to get the user name.
+
+
+## Installing
+
+```
+$ pip install viratoken
+```
+
+## Usage
+
+Having the private key you can encode and decode tokens:
+
+```python
+from viratoken import SignerVerifier
+
+sv = SignerVerifier(priv_key_path="settings/key",
+                    priv_key_password="settings/keypass")
+
+# Token payload
+token_data = {
+    'username': 'bafafa',
+    'more_data': 'rocambole',
+}
+
+# Token expiration time in minutes
+exp_minutes = 60
+
+encoded_token = sv.encode(token_data, exp_minutes)
+
+decoded_token = sv.decode(encoded_token)
+```
+
+Having only the public key, you can only decode:
+
+```python
+sv = sv.config(pub_key_path="settings/keypub")
+decoded_token = sv.decode(encoded_token)
+```
+
+You can also declare the `SignerVerifier` first and configure later:
+
+```python
+sv = SignerVerifier()
+sv.config(priv_key_path="settings/key",
+          priv_key_password="settings/keypass")
+```
--- setup.py
+++ setup.py
@@ -5,17 +5,33 @@ from setuptools import setup
 
 setup(
     name="viratoken",
-    version='0.0.1',
-    # url='',
+    version='0.1',
     description='Small lib to encode/decode/sign/verify JWTs '
                 'using asymmetric cryptography.',
     author='Andrés M. R. Martano',
-    # author_email='',
+    author_email='andres@inventati.org',
+    url='https://gitlab.com/ok-br/viratoken',
     packages=["viratoken"],
     install_requires=[
         'cryptography',
         'pyjwt',
     ],
-    # classifiers=[
-    # ]
+    keywords=['cryptography', 'jwt'],
+    classifiers=[
+        # "Development Status :: 3 - Alpha"
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+        # "Topic :: Internet :: WWW/HTTP"
+        # "Topic :: Internet :: WWW/HTTP :: Session",
+        # "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards",
+    ]
 )
