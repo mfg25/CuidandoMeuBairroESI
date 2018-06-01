aa42d4e4c81c5fe11e4aa316c2e863f2e27fa486
AndresMRM <andres@inventati.org>
2018-06-01 14:30:51 -0300

    log update to file

--- settings/templates/update.sh
+++ settings/templates/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $(dirname $BASH_SOURCE)
-%(venv_folder)s/bin/flask run_browser
-%(venv_folder)s/bin/flask send_notifications
+%(venv_folder)s/bin/flask run_browser >> %(logs_folder)s/update.log
+%(venv_folder)s/bin/flask send_notifications >> %(logs_folder)s/update.log
